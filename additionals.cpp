f40813810f00f88f2cc05b24a1518d6af540c871
LAnastasiia <lazorenko@ucu.edu.ua>
2019-05-23 16:54:29 +0300

    read-sort-write

--- additionals.cpp
+++ additionals.cpp
@@ -1,60 +1,14 @@
-//
-// Created by Sofiia Tesliuk on 2019-04-23.
-//
 
 #include "additionals.h"
 
 
-std::vector<std::string> parse_config_file(std::string config_filename){
-    std::vector<std::string> config;
-    //TODO
-    return config;
+// Return string - extention of the filename.
+std::string get_file_ext(const std::string &file_name){
+    std::size_t dot_ind = file_name.find_last_of('.');
+    return file_name.substr(dot_ind);
 }
 
-
-std::string get_txt_file(const std::string* unknown){
-    std::string valid_txt;
-    //TODO
-    return valid_txt;
-}
-
-
-std::string read_file(const std::string* unknown_data_filename){
-    std::string words;
-
-    std::string data_filename = get_txt_file(unknown_data_filename);
-    //TODO
-
-    return words;
-}
-
-
-std::vector<std::string> cut_words(const std::string* file){
-    std::vector<std::string> words;
-    //TODO
-    return words;
-}
-
-
-std::map<std::string, int> count_same_words(const std::vector<std::string> words){
-    std::map<std::string, int> counted_words;
-    //TODO
-    return counted_words;
-}
-
-
-bool by_alphabet(const std::string* w1, const std::string* w2){
-    //TODO
-    return true;
-}
-
-
-bool by_number(const std::string* w1, const std::string* w2){
-    //TODO
-    return true;
-}
-
-
-void write_results(const std::string* out_filename, const std::map<std::string, int>* results){
-    //TODO
+// Check if string can be converted to float.
+int is_file_ext(const std::string &file_name, const std::string &ext){
+    return !file_name.compare(file_name.length()-ext.length(), ext.length(), ext);
 }
---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <fstream>
+
+#include <boost/locale.hpp>
+//#include <boost/locale/boundary/segment.hpp>
+//#include <boost/locale/boundary/index.hpp>
+
+#include "additionals.h"
+#include "config.h"
+
+
+int main(int argc, char *argv[]){
+
+    std::string conf_file_name, to_file_alph, to_file_occur;
+
+    if(argc > 1){
+        conf_file_name = argv[1];
+    } else { conf_file_name = "config.dat"; }
+
+    if (! is_file_ext(conf_file_name, ".txt") && ! is_file_ext(conf_file_name, ".dat") ) {
+        std::cerr << "Wrong config file extention." << std::endl;
+        return -1;
+    }
+
+    // config
+    MyConfig mc;
+    mc.load_configs_from_file(conf_file_name);
+    if (mc.is_configured()) {
+        std::cout << "YES! Configurations loaded successfuly.\n" << std::endl;
+    } else { std::cerr << "Error. Not all configurations were loaded properly."; return -3;}
+
+
+    // read
+    std::ifstream in_f(mc.in_file);
+    if (! in_f.is_open() || in_f.rdstate())
+        { std::cerr << "Couldn't open input-file."; return -2; }
+
+    std::string content; std::stringstream ss;
+    ss << in_f.rdbuf();
+    content = ss.str();
+
+
+    using namespace boost::locale::boundary;
+
+    std::locale loc = boost::locale::generator().generate("en_US.UTF-8");
+    std::string text = "To e e e e  that be oR nOt to Be, that is! th?e question.";
+
+    // fold case
+    text = boost::locale::to_lower(text, loc);
+
+    // words to map
+    std::unordered_map<std::string, int> cut_words;
+    ssegment_index map(word, text.begin(), text.end(), loc);
+    map.rule(word_any);
+
+    for (ssegment_index::iterator it = map.begin(), e = map.end(); it != e; ++it) {
+        cut_words[*it]++;
+    }
+
+
+    using namespace std;
+
+    // sort & write
+    std::vector<std::pair<std::string, int> > vector_words;
+    for (auto &word: cut_words){
+        vector_words.emplace_back(word);
+    }
+
+    std::sort(vector_words.begin(), vector_words.end(), [](const auto t1, const auto t2){ return t1.second < t2.second;});
+    ofstream num_out_f(mc.to_numb_file);
+    for (auto &v : vector_words) {
+        num_out_f << v.first << ": " << std::to_string(v.second) + "\t";
+    }
+
+    std::sort(vector_words.begin(), vector_words.end(), [](const auto t1, const auto t2){ return t1.first.compare(t2.first)<0;});
+    ofstream alp_out_f(mc.to_alph_file);
+    for (auto &v : vector_words) {
+        alp_out_f << v.first << ": " << std::to_string(v.second) + "\t";
+    }
+
+
+    std::cout << "\nOK\n" << std::endl;
+    return 0;
+}
+}
